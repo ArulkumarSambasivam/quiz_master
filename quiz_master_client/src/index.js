d65f0b49b82b59139c60e80d3b401b090f88f9c7
ArulkumarSambasivam <arulkumarpmp@gmail.com>
2020-03-08 22:15:52 +0530

    basic mobx setup

---
+++ quiz_master_client/src/index.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { observable, configure, action } from "mobx";
+import { observer } from "mobx-react";
+import './index.css';
+import App from './App';
+import * as serviceWorker from './serviceWorker';
+configure({enforceActions:true});
+const appState= observable({count:0,
+incCount: action(()=>{
+    appState.count +=1;
+}),
+decCount: action(()=>{
+    appState.count -=1;
+})}
+);
+const Counter = observer( props => (<section>{props.appState.count}
+<div>
+<button onClick={props.appState.incCount}>Add</button>
+<button onClick={props.appState.decCount}>Dec</button>
+</div>
+</section>))
+ReactDOM.render(<Counter appState={appState} />, document.getElementById('root'));
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.unregister();
--- quiz_master_server/src/admin-controller.js
+++ quiz_master_server/src/admin-controller.js
@@ -1,37 +1,128 @@
 const admin = require('express').Router();
 const db = require("./db");
-admin.post('/addOrganisation', function(req, res) {
+admin.post('/addOrganisation', function (req, res) {
     let name = req.param("name");
     let email = req.param("email");
-    db.query('INSERT INTO organisation_master (name,email) VALUES (?,?)', 
-      [name,email], 
-      function (err, rows, fields) {
-          if(err){
-            console.log('Connection result error ' + err);     
-          }else{
-              res.send(rows);
-          }
-          
-      ;
-      }
+    db.query('INSERT INTO organisation_master (name,email) VALUES (?,?)',
+        [name, email],
+        function (err, rows, fields) {
+            if (err) {
+                console.log('Connection result error ' + err);
+            } else {
+                res.send(rows);
+            }
+        }
     );
-   
 });
 
-admin.post('/addClass', function(req, res) {
+admin.post('/addClass', function (req, res) {
+    let name = req.param("name");
+    let email = req.param("email");
+    let org_id = req.param("org_id");
+    db.query('INSERT INTO class_master (name,email,org_id) VALUES (?,?,?)',
+        [name, email, org_id],
+        function (err, rows, fields) {
+            if (err) {
+                console.log('Connection result error ' + err);
+            } else {
+                res.send(rows);
+            }
+        }
+    );
 
-    res.send("add class"+req.param("name"));
-   });
+});
 
-admin.post('/addUser', function(req, res) {
-    res.send("add User"+req.param("name"));
-   });
+admin.post('/addUser', function (req, res) {
+    let name = req.param("name");
+    let email = req.param("email");
+    let password = req.param("password");
+    let class_id = req.param("class_id");
+    let org_id = req.param("org_id");
+    db.query('INSERT INTO user_master (name,email,password,class_id,org_id) VALUES (?,?,?,?,?)',
+        [name, email, password, class_id, org_id],
+        function (err, rows, fields) {
+            if (err) {
+                console.log('Connection result error ' + err);
+            } else {
+                res.send(rows);
+            }
+        }
+    );
 
-admin.post('/addQuiz', function(req, res) {
-    res.send("add quiz"+req.param("name"));
-   });
+});
 
-admin.post('/addQuestion', function(req, res) {
-    res.send("add question"+req.param("name"));
-   });
+admin.post('/addAdmin', function (req, res) {
+    let name = req.param("name");
+    let email = req.param("email");
+    let password = req.param("password");
+    let org_id = req.param("org_id");
+    db.query('INSERT INTO admin_master (name,email,password,org_id) VALUES (?,?,?,?)',
+        [name, email, password, org_id],
+        function (err, rows, fields) {
+            if (err) {
+                console.log('Connection result error ' + err);
+            } else {
+                res.send(rows);
+            }
+        }
+    );
+
+});
+
+admin.post('/addAdmin', function (req, res) {
+    let name = req.param("name");
+    let email = req.param("email");
+    let password = req.param("password");
+    let org_id = req.param("org_id");
+    db.query('INSERT INTO admin_master (name,email,password,org_id) VALUES (?,?,?,?)',
+        [name, email, password, org_id],
+        function (err, rows, fields) {
+            if (err) {
+                console.log('Connection result error ' + err);
+            } else {
+                res.send(rows);
+            }
+        }
+    );
+
+});
+
+
+admin.post('/addQuiz', function (req, res) {
+    let name = req.param("name");
+    let class_id = req.param("class_id");
+    let admin_id = req.param("admin_id");
+    db.query('INSERT INTO quiz_master (name,class_id,admin_id) VALUES (?,?,?)',
+        [name, class_id, admin_id],
+        function (err, rows, fields) {
+            if (err) {
+                console.log('Connection result error ' + err);
+            } else {
+                res.send(rows);
+            }
+        }
+    );
+
+});
+
+admin.post('/addQuestion', function (req, res) {
+    let quiz_id = req.param("quiz_id");
+    let admin_id = req.param("admin_id");
+    let questions = req.param("questions");
+    let options = req.param("options");
+    let answer = req.param("answer");
+    db.query('INSERT INTO question_master (quiz_id,admin_id,questions,options,answer) VALUES (?,?,?,?,?)',
+        [quiz_id, admin_id, questions, options, answer],
+        function (err, rows, fields) {
+            if (err) {
+                console.log('Connection result error ' + err);
+            } else {
+                res.send(rows);
+            }
+
+            ;
+        }
+    );
+
+});
 module.exports = admin;
